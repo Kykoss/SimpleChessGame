901c918a241fdd29e6f6612d4b5821295760d1cb
RO <oldemeule@texware.de>
2022-07-12 18:52:28 +0200

    Initial Commit

---
+++ Board/BoardField.cs
@@ -0,0 +1,20 @@
+﻿using SimpleChessGame.Pieces;
+
+namespace SimpleChessGame.Board
+{
+    internal class BoardField
+    {
+        internal int XCoordinate { get; private set; }
+        internal int YCoordinate { get; private set; }
+        public string Name { get; private set; }
+        internal int Color { get; private set; }
+        internal Piece Piece { get; private set; }
+
+        public BoardField(float formXCoord, float formYCoord, int posX, int posY)
+        {
+            this.XCoordinate = posX;
+            this.YCoordinate = posY;
+
+        }
+    }
+}
---
+++ Pieces/Piece.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleChessGame.Pieces
+{
+    public enum Direction {Horizontal, Vertical, Diagonal}
+
+    internal abstract class Piece
+    {
+        public int MoveRange { get; private set; }
+        public Direction[] MoveDirections { get; private set; }
+
+
+    }
+}
